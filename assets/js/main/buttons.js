c42336fae49e67fc0b75903e4e112b531377a2e0
andy-and-terry <yucheng.lee29@gmail.com>
2026-02-21 20:33:12 +0800

    Update buttons.js

--- assets/js/main/buttons.js
+++ assets/js/main/buttons.js
@@ -1,1 +1,7 @@
+document.getElementById("login").addEventListener("click", () => {
+  window.open("https://andy-and-terry.github.io/learningforeveryone/login/", "_blank");
+});
 
+document.getElementById("more").addEventListener("click", () => {
+  window.open("https://andy-and-terry.github.io/learningforeveryone/more", "_blank");
+});
